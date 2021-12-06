f2adb047bd9f679d4031de84c8c84368d5c7056f
unknown <Saitharala@gmail.com>
2021-12-07 01:08:41 +0530

    [Tharala Sai Prashanth] Added Retrieve Top2 Above 60 Feauture

--- Lambda/Lambda/Program.cs
+++ Lambda/Lambda/Program.cs
@@ -9,14 +9,22 @@ namespace Lambda
         static void Main(string[] args)
         {
             List<Person> listPersonsInCity = new List<Person>();
-            AddingRecords(listPersonsInCity);
+            AddRecords(listPersonsInCity);
+            Retrieve_TopTwo_Above60(listPersonsInCity);
         }
-        private static void AddingRecords(List<Person> listPersonsInCity)
+        private static void AddRecords(List<Person> listPersonsInCity)
         {
             listPersonsInCity.Add(new Person("111111", "TSP", "TL", 22));
             listPersonsInCity.Add(new Person("222222", "SPT", "AP", 55));
             listPersonsInCity.Add(new Person("333333", "SP", "BG", 66));
             listPersonsInCity.Add(new Person("444444", "TP", "US", 77));
+        }
+        private static void Retrieve_TopTwo_Above60(List<Person> listPersonsInCity)
+        {
+            foreach (Person person in listPersonsInCity.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name: {0} Age: {1}", person.Name, person.Age);
+            }
         }
     }
 }
